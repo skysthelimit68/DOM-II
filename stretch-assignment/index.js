c537826417e2dfc2fe3cc383f2fce8f7c20c2ce7
Ming Liu <mliu@macpro.home>
2019-04-09 18:18:53 -0400

    some modification, still buggy

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,26 +1,27 @@
 let blocks = document.querySelectorAll(".block");
-let moveupCounter = 0;
+let moveupCounter = -1;
 let distance;
+
 blocks.forEach(elem => {
-    elem.addEventListener("click", (event) => {
-        event.target.setAttribute("style", `order: ${moveupCounter};`);
-        moveupCounter--;
-    })
     elem.addEventListener("mousedown", (event) => {
-        if(distance){
-            clearInterval(distance);
-        }
         let time = 0;  
         distance = setInterval(function(){
             time++;
             event.target.setAttribute("style", `transform:translateX(${time}px)`);
             if(time >= 800) {
                 clearInterval(distance);
-                event.target.setAttribute("style", "transform:translateX(0px)");
+                event.target.setAttribute("style", `transform:translateX(0px); order:${moveupCounter}`);
             }
         },0);
     })
+    elem.addEventListener("click", (event) => {
+        event.target.setAttribute("style", `order: ${moveupCounter};`);
+        moveupCounter--;
+    })
+    
     elem.addEventListener("mouseup", (event) => {
-             clearInterval(distance);
+        event.preventDefault()
+        event.target.setAttribute("style", `transform:translateX(0px); order:${moveupCounter}`);     
+        clearInterval(distance);
     })
 })
