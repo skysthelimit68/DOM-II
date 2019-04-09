791632f31cc94b65297da650922df8466213336a
Ming Liu <mliu@macpro.home>
2019-04-09 15:13:02 -0400

    setting up, first few before trying out gsap

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 // Your code goes here
+
+let destinations = document.querySelectorAll(".destination p");
+destinations.forEach(elem => {
+    elem.addEventListener("mouseover", (event) => {
+        event.target.style.color = "red";
+    });
+    elem.addEventListener("mouseleave", (event) => {
+        event.target.style.color = "black";
+    })
+})
+
+window.addEventListener("load", (event) => {
+    alert("Welcom to Fun Bus");
+})
+
+let allinputs = Array.from(document.querySelectorAll("form input")).concat(Array.from(document.querySelectorAll("form select"))).concat(Array.from(document.querySelectorAll("form textarea")));
+allinputs.forEach(elem => {
+    elem.addEventListener("focus", (event) => {
+        event.target.setAttribute("style", "background-color:yellow;");
+    });
+    elem.addEventListener("blur", (event) => {
+        event.target.setAttribute("style","background-color: white;");
+    });
+    elem.addEventListener("select", (event) => {
+        event.target.setAttribute("style", "font-weight:900; text-decoration:underline; font-style: italic;");
+    });
+})
+
