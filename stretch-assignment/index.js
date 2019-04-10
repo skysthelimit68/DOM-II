fa64fdff5cfbafea460763ba8d5e524c26c43393
Ming Liu <mliu@macpro.home>
2019-04-09 20:36:18 -0400

    fixed the but of out of order arrangement by adding data-value attribute to keep track of the order

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -3,20 +3,28 @@ let moveupCounter = -1;
 let distance;
 let target;
 let mouseDown = false;
+let order;
+
+blocks.forEach(elem => {
+    elem.setAttribute("data-value", `${moveupCounter}`);
+})
 
 blocks.forEach(elem => {
     elem.addEventListener("mousedown", (event) => {
-        mouseDown = true;
+        order = event.target.getAttribute("data-value");
+            console.log(order);
+            mouseDown = true;
         //event.preventDefault();
         moveupCounter--;
         target = event.target;
         let time = 0;  
         distance = setInterval(function(){
             time+=5;
-            event.target.setAttribute("style", `transform:translateX(${time*5}px);`);
+            
+            event.target.setAttribute("style", `transform:translateX(${time*5}px);order:${order};`);
             if(time >= 800) {
                 clearInterval(distance);
-                event.target.setAttribute("style", `transform:translateX(0px);`);
+                event.target.setAttribute("style", `transform:translateX(0px);order:${order};`);
             }
         },0);
     })
@@ -26,6 +34,7 @@ blocks.forEach(elem => {
             return;
         } else {
             event.target.setAttribute("style", `order: ${moveupCounter};`);
+            event.target.setAttribute("data-value", `${moveupCounter}`);
             moveupCounter--;
         }
         
@@ -33,7 +42,7 @@ blocks.forEach(elem => {
 })
     window.addEventListener("mouseup", (event) => {
         moveupCounter--;
-        target.setAttribute("style", `transform:translateX(0px);`);     
+        target.setAttribute("style", `transform:translateX(0px); order:${order};`);     
         clearInterval(distance);
         mouseDown = false;
     })
